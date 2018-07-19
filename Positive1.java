f1974348f88888a80cfd2f21a9e8b7f0d57be0f1
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-19 10:55:23 +0530

    Add files via upload

---
+++ Positive1.java
@@ -0,0 +1,22 @@
+
+public class Positive1 {
+
+	public static void main(String[] args) {
+		int var=0;
+		if(var>0)
+		{
+			System.out.println("POSITIVE");
+		}
+		else if(var<0)
+		{
+			System.out.println("NEGATIVE");
+		}
+		else
+		{
+			System.out.println("zero");
+		}
+
+
+	}
+
+}
