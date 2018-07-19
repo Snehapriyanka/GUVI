13acab4b947743a309849e68e49d988e5aa3eb2f
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-19 17:06:48 +0530

    Add files via upload

---
+++ Large5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.math.*;
+public class Large5 {
+
+	public static void main(String[] args) {
+		int a,b,c,large=0;
+		Scanner s=new Scanner(System.in);
+		 a=s.nextInt();
+		 b=s.nextInt();
+		 c=s.nextInt();
+		 if(a>b)
+			 large=a;
+			  if(b>c) 
+				 large=b;
+		 if (c>a)
+				 large=c;
+			 System.out.println("large"+ large);
+				 
+	}
+	}
+
+
+
