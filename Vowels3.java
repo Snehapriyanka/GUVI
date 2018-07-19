0a12bc56ed8832a2fda0646a0f4b803ccf22a8c3
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-19 13:55:36 +0530

    Add files via upload

---
+++ Vowels3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Vowels3 {
+
+	public static void main(String[] args) {
+		boolean isVowels=false;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the character");
+		int ch=s.next().charAt(0);
+		switch(ch)
+		{
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':isVowels=true;
+			
+		}
+		if(isVowels==true)
+			System.out.println("vowels");
+		else if((ch>='a'&&ch<='z') || (ch>='A'&& ch<='Z'))
+			System.out.println("constant");
+		else
+			System.out.println("invalid");
+
+	}
+
+}
