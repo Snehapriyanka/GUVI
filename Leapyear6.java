1209c317412a594915577083d0d5a7c1861cc561
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-19 17:33:29 +0530

    Add files via upload

---
+++ Leapyear6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Leapyear6 {
+
+	public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n%4==0)
+System.out.println("leapyear");
+if(n%4!=0)
+	System.out.println("no");
+	}
+
+}
