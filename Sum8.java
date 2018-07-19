bbdb4549a2aace56701916d39843b52ce4df48fa
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-19 18:19:52 +0530

    Add files via upload

---
+++ Sum8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Sum8 {
+
+	public static void main(String[] args) {
+Scanner c=new Scanner(System.in);
+int a=c.nextInt();
+int sum=0;
+for(int i=1;i<=a;i++)
+{
+sum=sum+i;
+}
+if(sum<9)
+	System.out.println(sum);
+else
+	System.out.println(sum);
+}
+	}
+
+
